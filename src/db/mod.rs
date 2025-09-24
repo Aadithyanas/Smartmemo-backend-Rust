30ed86b3cf0ffb30b69be5ecdf77369e23bee4b7
Aadithyan <rak34316@gmail.com>
2025-09-24 17:17:58 +0530

    fixxed build errors

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -1,6 +1,6 @@
 use sea_orm::{Database, DbConn, DbErr};
 
 pub async fn connect() -> Result<DbConn, DbErr> {
-    let url = "postgres://postgres:mark42@localhost:5432/memo"; // replace with your real credentials
+    let url = "postgres://memo_mrfj_user:lo4eJLOmcgBEgsI5LbTwh4ju5OJWb5Vr@dpg-d396progjchc73dh5mr0-a.oregon-postgres.render.com/memo"; // replace with your real credentials
     Database::connect(url).await
 }
